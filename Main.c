439d6857426bc166e6cdcfecffb34149867b8045
isab3lla-jl <isabella.juarezl111@gmail.com>
2025-10-09 23:28:23 -0600

    singly linked list como referencia e intento de agregar folder para debug

--- Main.c
+++ Main.c
@@ -1,2 +1,84 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+//lista ligada/linked list, datos ligados por su direccion
+typedef struct Node
+{
+    int value;
+    struct Node *next;
+} Node;
+
+
+Node* create_Node(int val)
+{
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->value = val;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Node *list = NULL;
+
+void try_add_Node(Node* n, Node* into)
+{
+    if(list == NULL){
+        list = n;
+        return;
+    }
+    if(into->next == NULL){
+        into->next = n;
+    }
+    else{
+        try_add_Node(n, into->next);
+    }
+}
+
+void free_list(Node* li)
+{
+    while(li){
+        Node* temp = li;
+        li = li->next;
+        free(temp);
+    }
+}
+
+void delete_Node()
+{
+
+    if(list == NULL){
+        printf("No hay elementos en la lista");
+        return;
+    }
+    if(list->next == NULL){
+        printf("Elemento unico");
+        Node *temp = list;
+        list = NULL;
+        free(temp);
+        return;
+    }
+    Node *penultimo = list;
+    while(penultimo->next->next){
+        penultimo = penultimo->next;
+    }
+    Node *temp = penultimo->next;
+    penultimo->next = NULL;
+    free(temp);
+    printf("Ultimo elemento borrado\n");
+}
+
+int main()
+{
+    try_add_Node(create_Node(10), list);
+    try_add_Node(create_Node(11), list);
+    try_add_Node(create_Node(12), list);
+    printf("Node0: %d \n", list->value);
+    printf("Node1: %d \n", list->next->value);
+    printf("Node2: %d \n", list->next->next->value);
+    delete_Node(list); 
+    printf("Node0: %d \n", list->value);
+    printf("Node1: %d \n", list->next->value);
+    //printf("Node2: %d \n", list->next->next->value);
+
+    free_list(list);
+    return 0;
+}
